b94ed03b4c8d15b93f24b7ef11cbefa3eaaff82e
Dale Syth <dalesyth@gmail.com>
2024-04-16 21:33:01 -0500

    Integrated TypeORM

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 // Instantiate express app
 const app: Express = express();
 dotenv.config();
+
+// Create Database Connection
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 
 // Define server port
 const port = process.env.PORT;
@@ -13,5 +25,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-// Start listening to the requests on the defined port
-app.listen(port);
+AppDataSource.initialize().then(() => {
+  app.listen(port);
+  console.log("Data Source has been initialized!");
+}).catch((err) => {
+  console.error('Error during Data Source initialization', err);
+});
