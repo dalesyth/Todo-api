57c5a50ba23304e86f07ee2f311e754b9e305e8e
Dale Syth <dalesyth@gmail.com>
2024-04-17 20:59:47 -0500

    Debugged endpoint post test

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import { TasksController } from "./tasks.controller";
+import { createValidator } from "./tasks.validator";
+import { validationResult } from "express-validator";
 
 // Fire the router function
 export const tasksRouter: Router = Router();
@@ -11,6 +13,15 @@ tasksRouter.get("/tasks", async (req: Request, res: Response) => {
   res.json(allTasks).status(200);
 });
 
-tasksRouter.post('/tasks', async (req: Request, res: Response)=>{
-    
-})
+tasksRouter.post(
+  "/tasks",
+  createValidator,
+  //@ts-ignore
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
