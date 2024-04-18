a16515ddd72b54d29a234fd3f800f2b5e6d4582e
Dale Syth <dalesyth@gmail.com>
2024-04-17 20:40:43 -0500

    Created tasks validator

--- src/tasks/tasks.validator.ts
+++ src/tasks/tasks.validator.ts
@@ -1,5 +1,6 @@
 import { body } from "express-validator";
 import { Priority } from "../enums/Priority";
+import { Status } from "../enums/Status";
 
 export const createValidator = [
   body("title")
@@ -10,9 +11,25 @@ export const createValidator = [
     .isString()
     .withMessage("Title needs to be in text format"),
 
-  body('date').not().isEmpty().withMessage('The task date is mandatory').isString().withMessage('The date needs to be a valid date format'),
+  body("date")
+    .not()
+    .isEmpty()
+    .withMessage("The task date is mandatory")
+    .isString()
+    .withMessage("The date needs to be a valid date format"),
 
-  body('description').trim().isString().withMessage('Description needs to be in text format'),
+  body("description")
+    .trim()
+    .isString()
+    .withMessage("Description needs to be in text format"),
 
-  body('priority').trim().isIn([Priority.normal, Priority.high, Priority.low])
+  body("priority")
+    .trim()
+    .isIn([Priority.normal, Priority.high, Priority.low])
+    .withMessage("Priority can only be high, normal, or low"),
+
+  body("status")
+    .trim()
+    .isIn([Status.completed, Status.inProgress, Status.todo])
+    .withMessage("Status can only be completed, inProgress, or completed"),
 ];
